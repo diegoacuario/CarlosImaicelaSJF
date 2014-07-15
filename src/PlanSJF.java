519204842ec5e568901ddc6273802a9e79c608a1
Ayleen <Ayleen@Ayleen-pc>
2014-07-15 00:01:13 -0500

    Version inicial de SJF

---
+++ src/PlanSJF.java
@@ -0,0 +1,70 @@
+
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+/**
+ *
+ * @author Carlos Imaicela
+ */
+public class PlanSJF {
+
+    public ArrayList<Proceso> ordenarPorTiempoRafaga(ArrayList<Proceso> proc) {
+        for (int i = 0; i < proc.size() - 1; i++) {
+            for (int j = i + 1; j < proc.size(); j++) {
+                if (proc.get(i).getTiempo() > proc.get(j).getTiempo()) {
+                    Proceso aux = proc.get(i);
+                    proc.set(i, proc.get(j));
+                    proc.set(j, aux);
+                }
+            }
+        }
+        return proc;
+    }
+
+    public double tiempoPromedio(ArrayList<Proceso> procesos) {
+        double tPro = tiempoTotal(procesos);
+        tPro /= procesos.size();
+        return tPro;
+    }
+
+    public int tiempoTotal(ArrayList<Proceso> procesos) {
+        int tTo = 0;
+        for (Proceso proceso : procesos) {
+            tTo += proceso.getTiempo();
+        }
+        return tTo;
+    }
+
+    public boolean encuentraProceso(Proceso p, ArrayList<Proceso> procesos) {
+        for (Proceso proceso : procesos) {
+            if (proceso.getStrNombre().equals(p.getStrNombre())) {
+                System.out.println(proceso.getStrNombre());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void agregarADiagramaGant(JPanel jP, ArrayList<Proceso> procOrd) {
+        jP.removeAll();
+        jP.setLayout(new java.awt.GridLayout(1, procOrd.size(), 5, 5));
+        for (Proceso p : procOrd) {
+            JLabel jb = new JLabel("<html>" + p.getStrNombre() + "<br>" + p.getTiempo() + "seg</html>");
+            jb.setBorder(new LineBorder(Color.blue));
+            jP.add(jb);
+
+        }
+    }
+
+    public String[][] convertirAMatriz(ArrayList<Proceso> proc) {
+        String procesos[][] = new String[proc.size()][2];
+        for (int i = 0; i < proc.size(); i++) {
+            procesos[i][0] = proc.get(i).getStrNombre();
+            procesos[i][1] = proc.get(i).getTiempo() + "";
+        }
+        return procesos;
+    }
+}
---
+++ src/Proceso.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * @author Carlos Imaicela
+ */
+public final class Proceso {
+
+    private String strNombre;
+    private int tiempo;
+
+    public Proceso(String nombre, int tiempo) {
+        setStrNombre(nombre);
+        setTiempo(tiempo);
+    }
+
+    public String getStrNombre() {
+        return strNombre;
+    }
+
+    public void setStrNombre(String strNombre) {
+        this.strNombre = strNombre;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+}
